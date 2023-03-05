55da1f74a3e97fedaf7f09553ec1886e4dfa79b2
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2023-03-05 09:41:30 +0200

    fix: added store to header component

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,16 +1,36 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { Currency } from 'src/app/shared/models/currency.model';
+import { Store } from '@ngrx/store';
+import { UAH_ID } from 'src/app/utils/constants';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnChanges {
-  @Input() rates: Currency[];
+export class HeaderComponent implements OnInit, OnChanges {
+  rates: Currency[];
 
-  constructor() {}
+  constructor(
+    private store: Store<{ currencies: { currencies: Currency[] } }>
+  ) {}
+
+  ngOnInit(): void {
+    this.store.select('currencies').subscribe({
+      next: data => {
+        this.rates = data.currencies.filter(curr => curr.id !== UAH_ID);
+      },
+      error: error => {
+        console.error(error);
+      },
+    });
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     this.rates = changes['rates'].currentValue;
